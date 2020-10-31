18935d2d67ac619cfaa5829dd2bf3abec0560193
nychitman1 <jchasky@gmail.com>
2020-10-31 09:39:21 -0700

    Clean up some of the missing vendor packages

--- device-lineage.mk
+++ device-lineage.mk
@@ -67,10 +67,8 @@ PRODUCT_PACKAGES += \
 # Vendor packages
 PRODUCT_PACKAGES += \
     ese_spi_st \
-    libcamera2ndk_vendor \
     libcodec2_hidl@1.0.vendor:32 \
     libcodec2_vndk.vendor \
-    libdisplayconfig \
     libdrm.vendor \
     libjson \
     libmedia_ecoservice.vendor \
@@ -82,15 +80,13 @@ PRODUCT_PACKAGES += \
     libnos_datagram_citadel:64 \
     libnosprotos:64 \
     libnos_transport:64 \
-    libsensorndkbridge \
-    libtextclassifier_hash.vendor:64 \
+    libsensorndkbridge:64 \
     libtinycompress \
     libtinyxml \
     libwifi-hal-qcom \
     nos_app_avb:64 \
     nos_app_keymaster:64 \
     nos_app_weaver:64 \
-    sound_trigger.primary.sm6150 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@1.1.vendor \
     vendor.display.config@1.2.vendor \
