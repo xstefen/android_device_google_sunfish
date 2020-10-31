2a0c6e13e42def3e09c7d8379fb7cbc7f0792fd1
nychitman1 <jchasky@gmail.com>
2020-10-31 07:16:15 -0700

    Show LTE settings
    Co-authored-by: xstefen <x+github@stefen.me>

--- device-lineage.mk
+++ device-lineage.mk
@@ -33,6 +33,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor \
