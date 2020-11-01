c686dd2c48b45ea69912c883a373914af0bbe0e7
xstefen <x@stefen.me>
2020-10-31 20:55:32 -0500

    Adjust kernel build

--- BoardConfig-lineage.mk
+++ BoardConfig-lineage.mk
@@ -19,13 +19,13 @@
 TARGET_NEEDS_DTBOIMAGE := true
 
 # KERNEL
-BOARD_KERNEL_IMAGE_NAME := Image.lz4
-KERNEL_LD := LD=ld.lld
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/google/sunfish
 TARGET_KERNEL_CONFIG := sunfish_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_CLANG_VERSION := proton
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
 
 # VBMETA
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS="--set_hashtree_disabled_flag"
@@ -35,5 +35,7 @@ ifneq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
 BOARD_VENDORIMAGE_PARTITION_SIZE := 562024448
 endif
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES := true
 
 -include vendor/google/sunfish/BoardConfigVendor.mk
